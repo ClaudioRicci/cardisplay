d3a03976e0aae9567f73c9634aee600f1287567c
Claudio Ricci <claudioricci@Claudios-MBP.home>
2019-11-02 22:14:20 +0000

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State {
+  isLoading: boolean;
+  cars: [];
+  error: any;
 }
 
-export default App;
+export default class App extends React.Component {
+  state: Readonly<State> = {
+    isLoading: true,
+    cars: [],
+    error: null
+  };
+
+  fetchCars() {
+    fetch(`https://warm-dawn-92320.herokuapp.com/models`)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          cars: data,
+          isLoading: false
+        })
+      )
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  componentDidMount() {
+    this.fetchCars();
+  }
+  render() {
+    const { isLoading, cars, error } = this.state;
+    return (
+      <React.Fragment>
+        <h1>Random Car</h1>
+        {error ? <p>{error.message}</p> : null}
+        {!isLoading ? (
+          cars.map(car => {
+            const { id, make, model } = car;
+            return (
+              <div key={id}>
+                <p>Make: {make}</p>
+                <p>Model: {model}</p>
+                <hr />
+              </div>
+            );
+          })
+        ) : (
+          <h3>Loading...</h3>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
