810e8a9f2a080eac1a4375612c31785dbe1c38fd
Claudio Ricci <claudioricci@Claudios-MBP.home>
2019-11-08 23:01:49 +0000

    styling desktop

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -49,165 +49,53 @@ export default function Grid() {
                           <div className="rightSection">
                             <p>{make}</p>
                             <p>{model}</p>
-                            <p>&pound;{rrp}</p>
+                            <p className="price">&pound;{rrp}</p>
                           </div>
                         </div>
-
-                        <span
-                          className={
-                            carwow_rating > 0
-                              ? "fa fa-star checked"
-                              : "fa fa-star"
-                          }
-                        ></span>
-                        <span
-                          className={
-                            carwow_rating > 2
-                              ? "fa fa-star checked"
-                              : "fa fa-star"
-                          }
-                        ></span>
-                        <span
-                          className={
-                            carwow_rating > 4
-                              ? "fa fa-star checked"
-                              : "fa fa-star"
-                          }
-                        ></span>
-                        <span
-                          className={
-                            carwow_rating > 6
-                              ? "fa fa-star checked"
-                              : "fa fa-star"
-                          }
-                        ></span>
-                        <span
-                          className={
-                            carwow_rating > 8
-                              ? "fa fa-star checked"
-                              : "fa fa-star"
-                          }
-                        ></span>
+                        <div className="carwowRating">
+                          <h3>Carwow Rating</h3>
+                          <span
+                            className={
+                              carwow_rating > 0
+                                ? "fa fa-star checked"
+                                : "fa fa-star"
+                            }
+                          ></span>
+                          <span
+                            className={
+                              carwow_rating > 2
+                                ? "fa fa-star checked"
+                                : "fa fa-star"
+                            }
+                          ></span>
+                          <span
+                            className={
+                              carwow_rating > 4
+                                ? "fa fa-star checked"
+                                : "fa fa-star"
+                            }
+                          ></span>
+                          <span
+                            className={
+                              carwow_rating > 6
+                                ? "fa fa-star checked"
+                                : "fa fa-star"
+                            }
+                          ></span>
+                          <span
+                            className={
+                              carwow_rating > 8
+                                ? "fa fa-star checked"
+                                : "fa fa-star"
+                            }
+                          ></span>
+                        </div>
+                        <a className="button" href={model} title={model}>
+                          <span>Get Offer</span>
+                        </a>
                       </div>
-                      <a className="button" href={model} title={model}>
-                        <span>Get Offer</span>
-                      </a>
                     </article>
                   </>
-
-                  // <div className="flex-container">
-                  //   <div key={id} className="card">
-                  //     <img
-                  //       src={img_url}
-                  //       alt={make + ": " + model + ", RRP: " + rrp}
-                  //     />
-                  //     <div className="detailsSurround">
-                  //       <div className="leftSectionSurround">
-                  //         <div className="leftSection">
-                  //           <h2>Make:</h2>
-                  //           <h2>Model:</h2>
-                  //           <h2>RRP:</h2>
-                  //         </div>
-                  //       </div>
-                  //       <div className="rightSectionSurround">
-                  //         <div className="rightSection">
-                  //           <p>{make}</p>
-                  //           <p>{model}</p>
-                  //           <p>&pound;{rrp}</p>
-                  //         </div>
-                  //       </div>
-                  //     </div>
-                  //     <span
-                  //       className={
-                  //         carwow_rating > 0
-                  //           ? "fa fa-star checked"
-                  //           : "fa fa-star"
-                  //       }
-                  //     ></span>
-                  //     <span
-                  //       className={
-                  //         carwow_rating > 2
-                  //           ? "fa fa-star checked"
-                  //           : "fa fa-star"
-                  //       }
-                  //     ></span>
-                  //     <span
-                  //       className={
-                  //         carwow_rating > 4
-                  //           ? "fa fa-star checked"
-                  //           : "fa fa-star"
-                  //       }
-                  //     ></span>
-                  //     <span
-                  //       className={
-                  //         carwow_rating > 6
-                  //           ? "fa fa-star checked"
-                  //           : "fa fa-star"
-                  //       }
-                  //     ></span>
-                  //     <span
-                  //       className={
-                  //         carwow_rating > 8
-                  //           ? "fa fa-star checked"
-                  //           : "fa fa-star"
-                  //       }
-                  //     ></span>
-                  //     <a href={model} title={model}>
-                  //       Get Offer
-                  //     </a>
-                  //   </div>
-                  // </div>
-
-                  // <div key={id} className="card">
-                  //   <img
-                  //     src={img_url}
-                  //     alt={make + ": " + model + ", RRP: " + rrp}
-                  //   />
-                  //   <div className="detailsSurround">
-                  //     <div className="leftSectionSurround">
-                  //       <div className="leftSection">
-                  //         <h2>Make:</h2>
-                  //         <h2>Model:</h2>
-                  //         <h2>RRP:</h2>
-                  //       </div>
-                  //     </div>
-                  //     <div className="rightSectionSurround">
-                  //       <div className="rightSection">
-                  //         <p>{make}</p>
-                  //         <p>{model}</p>
-                  //         <p>&pound;{rrp}</p>
-                  //       </div>
-                  //     </div>
-                  //   </div>
-                  //   <span
-                  //     className={
-                  //       carwow_rating > 0 ? "fa fa-star checked" : "fa fa-star"
-                  //     }
-                  //   ></span>
-                  //   <span
-                  //     className={
-                  //       carwow_rating > 2 ? "fa fa-star checked" : "fa fa-star"
-                  //     }
-                  //   ></span>
-                  //   <span
-                  //     className={
-                  //       carwow_rating > 4 ? "fa fa-star checked" : "fa fa-star"
-                  //     }
-                  //   ></span>
-                  //   <span
-                  //     className={
-                  //       carwow_rating > 6 ? "fa fa-star checked" : "fa fa-star"
-                  //     }
-                  //   ></span>
-                  //   <span
-                  //     className={
-                  //       carwow_rating > 8 ? "fa fa-star checked" : "fa fa-star"
-                  //     }
-                  //   ></span>
-                  //   <a href={model} title={model}>
-                  //     Get Offer
-                  //   </a>
-                  // </div>
                 );
               })
             )}
