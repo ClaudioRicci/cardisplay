6f8754b6c0ccac3676c56f80211cc0c4728fd7cd
Claudio Ricci <claudioricci@Claudios-MBP.home>
2019-11-08 13:22:12 +0000

    add generalMethods

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,115 +1,103 @@
-import React from "react";
-import { CarData } from "../interfaces/interfaces";
-import axios from "axios";
-
+import React, { useState, useEffect } from "react";
+import { getItem } from "../methods/generalMethods";
+// import { CarData } from "../interfaces/interfaces";
 import "./grid.scss";
 
-class Grid extends React.Component {
-  state: Readonly<CarData> = {
-    isLoading: true,
-    cars: [],
-    error: null
-  };
+export default function Grid() {
+  const [cars, setCars] = useState(null);
+  const [load, setLoad] = useState(false);
+  const [error, setError] = useState(null);
 
-  fetchCars() {
-    axios
-      .get("https://warm-dawn-92320.herokuapp.com/models")
-      .then(response =>
-        this.setState({
-          cars: response.data,
-          isLoading: false
-        })
-      )
-      .then(error => {
-        this.setState({ error, isLoading: false });
+  useEffect(() => {
+    getItem("https://warm-dawn-92320.herokuapp.com/models")
+      .then(res => {
+        setCars(res);
+        setLoad(true);
+      })
+      .catch(err => {
+        setError(err);
+        setLoad(true);
       });
-  }
+  }, []);
 
-  componentDidMount() {
-    this.fetchCars();
-  }
-  render() {
-    const { isLoading, cars, error } = this.state;
+  if (load) {
     return (
-      <>
-        <main className="main-area">
-          <div className="centered">
-            <div className="cards">
-              {error ? <p>{error.message}</p> : null}
-              {!isLoading ? (
-                cars.map(car => {
-                  const { id, make, model, img_url, rrp, carwow_rating } = car;
-                  return (
-                    <div key={id} className="card">
-                      <a href={model} title={model}>
-                        <img
-                          src={img_url}
-                          alt={make + ": " + model + ", RRP: " + rrp}
-                        />
-                        <div className="detailsSurround">
-                          <div className="leftSectionSurround">
-                            <div className="leftSection">
-                              <h2>Make:</h2>
-                              <h2>Model:</h2>
-                              <h2>RRP:</h2>
-                            </div>
-                          </div>
-                          <div className="rightSectionSurround">
-                            <div className="rightSection">
-                              <p>{make}</p>
-                              <p>{model}</p>
-                              <p>&pound;{rrp}</p>
-                            </div>
+      <main className="main-area">
+        <div className="centered">
+          <div className="cards">
+            {error ? (
+              <h1>{error.message}</h1>
+            ) : (
+              cars.map(car => {
+                const { id, make, model, img_url, rrp, carwow_rating } = car;
+                return (
+                  <div key={id} className="card">
+                    <a href={model} title={model}>
+                      <img
+                        src={img_url}
+                        alt={make + ": " + model + ", RRP: " + rrp}
+                      />
+                      <div className="detailsSurround">
+                        <div className="leftSectionSurround">
+                          <div className="leftSection">
+                            <h2>Make:</h2>
+                            <h2>Model:</h2>
+                            <h2>RRP:</h2>
                           </div>
                         </div>
-                        <span
-                          className={
-                            carwow_rating > 0
-                              ? "fa fa-star checked"
-                              : "fa fa-star"
-                          }
-                        ></span>
-                        <span
-                          className={
-                            carwow_rating > 2
-                              ? "fa fa-star checked"
-                              : "fa fa-star"
-                          }
-                        ></span>
-                        <span
-                          className={
-                            carwow_rating > 4
-                              ? "fa fa-star checked"
-                              : "fa fa-star"
-                          }
-                        ></span>
-                        <span
-                          className={
-                            carwow_rating > 6
-                              ? "fa fa-star checked"
-                              : "fa fa-star"
-                          }
-                        ></span>
-                        <span
-                          className={
-                            carwow_rating > 8
-                              ? "fa fa-star checked"
-                              : "fa fa-star"
-                          }
-                        ></span>
-                      </a>
-                    </div>
-                  );
-                })
-              ) : (
-                <h1>Cars Loading...</h1>
-              )}
-            </div>
+                        <div className="rightSectionSurround">
+                          <div className="rightSection">
+                            <p>{make}</p>
+                            <p>{model}</p>
+                            <p>&pound;{rrp}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <span
+                        className={
+                          carwow_rating > 0
+                            ? "fa fa-star checked"
+                            : "fa fa-star"
+                        }
+                      ></span>
+                      <span
+                        className={
+                          carwow_rating > 2
+                            ? "fa fa-star checked"
+                            : "fa fa-star"
+                        }
+                      ></span>
+                      <span
+                        className={
+                          carwow_rating > 4
+                            ? "fa fa-star checked"
+                            : "fa fa-star"
+                        }
+                      ></span>
+                      <span
+                        className={
+                          carwow_rating > 6
+                            ? "fa fa-star checked"
+                            : "fa fa-star"
+                        }
+                      ></span>
+                      <span
+                        className={
+                          carwow_rating > 8
+                            ? "fa fa-star checked"
+                            : "fa fa-star"
+                        }
+                      ></span>
+                    </a>
+                  </div>
+                );
+              })
+            )}
           </div>
-        </main>
-      </>
+        </div>
+      </main>
     );
+  } else {
+    return <div>Loading...</div>;
   }
 }
-
-export default Grid;
